8ff350593ff1498ad580626d112a224eff43ba3b
spatulatom <spatulatom@gmail.com>
2023-02-11 11:09:19 +0000

    hhh

--- controllers/upload-cloudinary.js
+++ controllers/upload-cloudinary.js
@@ -2,7 +2,7 @@ import fs from 'fs';
 import crypto from 'crypto';
 import cloudinary from 'cloudinary';
 
-const newUpload = async(req, res, next) => {
+const newUpload = async (req, res, next) => {
   // Configuration
   cloudinary.v2.config({
     cloud_name: process.env.CLOUD_NAME,
@@ -14,7 +14,7 @@ const newUpload = async(req, res, next) => {
   let response;
   try {
     console.log('here');
-   response = await cloudinary.uploader.upload(req.file.path, {
+    response = await cloudinary.uploader.upload('https://eventsbook22.s3.eu-west-3.amazonaws.com/992d717a-6204-42c1-b439-b5729d54d5ab', {
       public_id: crypto.randomUUID(),
     });
   } catch (err) {
@@ -30,23 +30,23 @@ const newUpload = async(req, res, next) => {
   //     console.log('Cloudinary response', data);
   //     console.log(data.secure_url);
 
-      // fs.unlink(req.file.path, (err) => {
-      //   //  its not crucial so we wont stop the execution if insuccessfull
-      //   console.log(err);
-      //   return;
-      //   //   const error = new HttpError(
-      //   //     'Could not unlink the file.',
-      //   //     500
-      //   //   );
-      //   //   return next(error);
-      // });
-      res.status(201).json({ secure_url: response.secure_url });
+  // fs.unlink(req.file.path, (err) => {
+  //   //  its not crucial so we wont stop the execution if insuccessfull
+  //   console.log(err);
+  //   return;
+  //   //   const error = new HttpError(
+  //   //     'Could not unlink the file.',
+  //   //     500
+  //   //   );
+  //   //   return next(error);
+  // });
+  res.status(201).json({ secure_url: response.secure_url });
 
-      //  res.json({ url: result.Location });
-    // })
-    // .catch((err) => {
-    //   console.log('ErroRR', err);
-    //   return next(new HttpError(err, err.statusCode));
-    // });
+  //  res.json({ url: result.Location });
+  // })
+  // .catch((err) => {
+  //   console.log('ErroRR', err);
+  //   return next(new HttpError(err, err.statusCode));
+  // });
 };
 export default newUpload;
