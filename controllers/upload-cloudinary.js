d60dd0348f227561f64ed404e4108f647753871e
spatulatom <spatulatom@gmail.com>
2023-02-12 18:33:47 +0000

    fffff

--- controllers/upload-cloudinary.js
+++ controllers/upload-cloudinary.js
@@ -29,17 +29,17 @@ const newUpload = async (req, res, next) => {
     deletePicture = true;
     return next(err);
   }
-  // if(deletePicture){
-  //   console.log('here5')
-  //   fs.unlink(req.file.path, (err) => {
-  //     //  its not crucial so we wont stop the execution if insuccessfull
-  //     console.log(err);
-  //     //   const error = new HttpError(
-  //     //     'Could not unlink the file.',
-  //     //     500
-  //     //   );
-  //     //   return next(error);
-  //   });}
+  if(deletePicture){
+    console.log('here5')
+    fs.unlink(req.file.path, (err) => {
+      //  its not crucial so we wont stop the execution if insuccessfull
+      console.log(err);
+      //   const error = new HttpError(
+      //     'Could not unlink the file.',
+      //     500
+      //   );
+      //   return next(error);
+    });}
 
   console.log('here 3', response);
   res.status(201).json({ secure_url: response.secure_url });
