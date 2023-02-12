dfe958eefa2964cd26fc50bb59caf68c60bc6cb1
spatulatom <spatulatom@gmail.com>
2023-02-12 10:53:52 +0000

    try catch

--- controllers/upload-cloudinary.js
+++ controllers/upload-cloudinary.js
@@ -13,37 +13,24 @@ const newUpload = async (req, res, next) => {
 
   //  see familija repository for Amazon web Services S3 bucket connection
   let response;
-  let unlinkImage = false;
+  let error;
   try {
     console.log('here');
     response = await cloudinary.uploader.upload(req.file.path, {
       public_id: crypto.randomUUID(),
     });
-    console.log('here 3');
-    unlinkImage = true;
   } catch (err) {
-    const error = new HttpError(
-      'Creating new post failed, server error, please try again in a minute.',
-      500
-    );
-    unlinkImage = true;
-    return next(error);
+    error = err;
+    return next(err);
   }
-  setTimeout(()=>{  if (unlinkImage) {
-    console.log('here 2')
+  try {
     fs.unlink(req.file.path, (err) => {
       //  its not crucial so we wont stop the execution if insuccessfull
       console.log(err);
-
-      //   const error = new HttpError(
-      //     'Could not unlink the file.',
-      //     500
-      //   );
-      //   return next(error);
     });
-  }
-},3000)
- 
+  } catch (err) {}
+  
+  
 
   console.log('response', response);
   res.status(201).json({ secure_url: response.secure_url });
